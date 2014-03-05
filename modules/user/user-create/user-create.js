a0535697863e29ab755f85750473b7a39f455c68
Andre Venter <andre.n.venter@gmail.com>
2014-03-05 23:57:02 +0200

    add user-create partial functionality as part of CRUD

--- modules/user/user-create/user-create.js
+++ modules/user/user-create/user-create.js
@@ -1,4 +1,13 @@
-angular.module('user').controller('UserCreateCtrl',function($scope){
+angular.module('user').controller('UserCreateCtrl',function($scope,$location,userService){
 
+    $scope.addUser = function(user){
+        userService.createUser(user)
+            .success(function(data){
+                $location.path('/user-list');
+            })
+            .error(function(data){
+                console.log(data);
+            });
+    };
 
 });
